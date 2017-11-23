de0bd099dc35254eaf6110ec278eae9c9f3f7534
fuyujiao <fyj1256@qq.com>
2017-11-23 11:19:50 +0800

    Initial Commit

---
+++ FYJWave/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FYJWave
+//
+//  Created by GZHL on 2017/11/23.
+//  Copyright © 2017年 fyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FYJWave/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYJWave
+//
+//  Created by GZHL on 2017/11/23.
+//  Copyright © 2017年 fyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
